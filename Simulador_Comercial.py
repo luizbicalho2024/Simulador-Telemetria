675934090e92a4fffd5b9d4fd3ea2e873b9e3fa6
Bicalho <74017831+clientenocego@users.noreply.github.com>
2024-11-08 17:08:06 -0400

    08-11-31

--- Simulador_Comercial.py
+++ Simulador_Comercial.py
@@ -13,15 +13,16 @@ modeloPF = st.selectbox(
             ("GPRS / Gsm", "Satelital"))
 
 if modeloPF == "GPRS / Gsm":
-    st.write(f"### Valor Anual Unitário À Vista: R$ {gprs:,.2f}")
+    st.write(f"### Valor Anual À Vista: R$ {gprs:,.2f}")
+    desconto = st.checkbox("Efetuar Desconto: ",value=False)
+
+    if desconto:
+        porcetagem = st.text_input(f'Qual Porcentagem de Desconto:')
+        desconto_calc = gprs - (gprs * desconto/100)
+        st.write(f"### Valor Anual com Desconto: R$ {desconto_calc:,.2f}")
                 
 else:
-    st.write(f"### Valor Anual Unitário À Vista: R$ {satelite:,.2f}")
-
-desconto = st.checkbox("Efetuar Desconto: ",value=False)
-
-if desconto:
-    porcetagem = st.text_input(f'Qual Porcentagem de Desconto: {porcetagem} %')
+    st.write(f"### Valor Anual À Vista: R$ {satelite:,.2f}")
 
 parcelamento = st.checkbox("Deseja Parcelar: ",value=False)
 
@@ -40,6 +41,7 @@ if parcelamento:
         total = calc_par * int(option)
         st.write(f"{option} Parcelas de: R$ {calc_par:,.2f}")  
         st.write(f"### Valor Anual Unitário: R$ {total:,.2f}") 
+        
     else:
         option = st.selectbox(
                         "Deseja Parcelar: ",
