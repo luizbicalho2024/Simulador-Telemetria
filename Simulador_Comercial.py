e3fbfe65cad5b557eb488dd12605d7f3efc81746
Bicalho <74017831+clientenocego@users.noreply.github.com>
2024-11-08 16:50:10 -0400

    08-11-26

--- Simulador_Comercial.py
+++ Simulador_Comercial.py
@@ -6,23 +6,7 @@ st.markdown("## Simulador de Venda Verdio Pessoa Física")
 
 gprs = 970.56
 satelite = 2325.60
-
-
-def calc ():
-    option = st.selectbox(
-                    "Deseja Parcelar: ",
-                    ("2", "3", "4","5","6","7","8","9","10","11","12"),
-                    #index="Avista",
-                    placeholder="Selecione a quantidade de Parcelas",
-                )
-    st.write(f"Parcelamento: {option}x")
-    margem = int(option) * 0.0408
-    parce = gprs / int(option)
-    calc_par = parce + (parce * margem)
-    total = calc_par * int(option)
-    st.write(f"{option} Parcelas de: R$ {calc_par:,.2f}")  
-    st.write(f"### Valor Anual Unitário: R$ {total:,.2f}") 
-
+    
 
 modeloPF = st.selectbox(
             'Tipo de Rastreador: ',
@@ -37,5 +21,16 @@ else:
 parcelamento = st.checkbox("Deseja Parcelar: ",value=False)
 
 if parcelamento:
-    calc
-    
+    option = st.selectbox(
+                    "Deseja Parcelar: ",
+                    ("2", "3", "4","5","6","7","8","9","10","11","12"),
+                    #index="Avista",
+                    placeholder="Selecione a quantidade de Parcelas",
+                )
+    st.write(f"Parcelamento: {option}x")
+    margem = int(option) * 0.0408
+    parce = gprs / int(option)
+    calc_par = parce + (parce * margem)
+    total = calc_par * int(option)
+    st.write(f"{option} Parcelas de: R$ {calc_par:,.2f}")  
+    st.write(f"### Valor Anual Unitário: R$ {total:,.2f}") 
