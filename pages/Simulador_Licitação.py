cd7f00db79d9b57ff3d29593f33f1937afba80fe
Bicalho <74017831+clientenocego@users.noreply.github.com>
2025-02-04 16:08:34 -0400

    02135461

--- pages/Simulador_Licitação.py
+++ pages/Simulador_Licitação.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Configuração inicial
 logo = ("imgs/logo.png")
-st.image("imgs/logo.png", width=200, caption="Logo")
+st.image("imgs/logo.png", width=200, caption=" ")
 st.markdown("## Simulador para Licitações e Editais")
 
 # Tabela de preços
@@ -17,14 +17,16 @@ precoCusto = {
 # Entrada de dados
 qtd_input = st.text_input("Quantos Veículos deseja realizar cotação: ", value="1", key="qtd")
 contrato_input = st.text_input("Tempo de Contrato (meses): ", value="12", key="contrato")
+margem_input = st.text_input("Margem de Lucro Desejado: ", value="30", key="margem")
 
 # Validação de entradas
 try:
     qtd = int(qtd_input) if qtd_input.isdigit() else 1  # Converte para inteiro ou usa 1 como padrão
     contrato = int(contrato_input) if contrato_input.isdigit() else 12  # Converte para inteiro ou usa 12 como padrão
+    margem = int(margem_input) if margem_input.isdigit() else 30
 except ValueError:
     st.error("Por favor, insira valores numéricos válidos para quantidade e contrato.")
-    qtd, contrato = 1, 12  # Valores padrão em caso de erro
+    qtd, contrato = 1, 12, 30  # Valores padrão em caso de erro
 
 # Seleção de itens
 itens_selecionados = []
@@ -34,9 +36,11 @@ for item, preco in precoCusto.items():
 
 # Cálculo do valor total
 valor_total_unitario = sum(precoCusto[item] for item in itens_selecionados)
-valor_total = valor_total_unitario * qtd * contrato
+un_contrato = valor_total_unitario / contrato
+un_margem = un_contrato + (un_contrato * margem)
+valor_total = un_margem * qtd * contrato
 
 # Exibição do resultado
-st.write(f"## Valor total unitário: R$ {valor_total_unitario:,.2f}")
+st.write(f"## Valor total unitário: R$ {un_margem:,.2f}")
 st.write(f"## Valor total: R$ {valor_total:,.2f}")
 st.write(f"#### (considerando {qtd} veículos e {contrato} meses)")
