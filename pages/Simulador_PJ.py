9eb6992b8b7551977c37b80bdadd9387ac0438c4
Bicalho <74017831+clientenocego@users.noreply.github.com>
2025-03-24 10:28:44 -0400

    01345813524

--- pages/Simulador_PJ.py
+++ pages/Simulador_PJ.py
@@ -13,21 +13,21 @@ gprs12 = 80.88
 satelite12 = 193.80
 rfid = 19.25
 redecan = 75.25
-mdvr = 595.60
+mdvr = 409.11
 dms = 324.75
 
 gprs24 = 53.92
 satelite24 = 129.20
 rfid24 = 12.83
 redecan24 = 50.17
-mdvr24 = 397.07
+mdvr24 = 272.74
 dms24 = 216.50
 
 gprs36 = 44.93
 satelite36 = 107.67
 rfid36 = 10.69
 redecan36 = 41.81
-mdvr36 = 330.89
+mdvr36 = 227.28
 dms36 = 180.42
 
 st.text_input("Quantos Veículos deseja realizar cotação: ", value='1', key="qtd")
@@ -48,8 +48,8 @@ if temp == "12 Meses":
         f"Satélite R$ {satelite12:,.2f}": satelite12,
         f"Identificador de Motorista / RFID R$ {rfid:,.2f}": rfid,
         f"Leitor de Rede CAN / Telemetria R$ {redecan:,.2f}": redecan,
-        f"Videomonitoramento / MDVR R$ {mdvr:,.2f}":mdvr,
-        f"Sensor de Fadiga / DMS R$ {dms:,.2f}":dms,
+        f"Videomonitoramento + DMS + ADAS R$ {mdvr:,.2f}":mdvr,
+        #f"Sensor de Fadiga / DMS R$ {dms:,.2f}":dms,
     }
     for item, valor in checkboxes.items():
         if st.checkbox(item):
@@ -68,8 +68,8 @@ elif temp == "24 Meses":
         f"Satélite R$ {satelite24:,.2f}": satelite24,
         f"Identificador de Motorista / RFID R$ {rfid24:,.2f}": rfid24,
         f"Leitor de Rede CAN / Telemetria R$ {redecan24:,.2f}": redecan24,
-        f"Videomonitoramento / MDVR R$ {mdvr24:,.2f}":mdvr24,
-        f"Sensor de Fadiga / DMS R$ {dms24:,.2f}":dms24,
+        f"Videomonitoramento + DMS + ADAS R$ {mdvr24:,.2f}":mdvr24,
+        #f"Sensor de Fadiga / DMS R$ {dms24:,.2f}":dms24,
     }
     for item, valor in checkboxes.items():
         if st.checkbox(item):
@@ -87,8 +87,8 @@ else :
         f"Satélite R$ {satelite36:,.2f}": satelite36,
         f"Identificador de Motorista / RFID R$ {rfid36:,.2f}": rfid36,
         f"Leitor de Rede CAN / Telemetria R$ {redecan36:,.2f}": redecan36,
-        f"Videomonitoramento / MDVR R$ {mdvr36:,.2f}":mdvr36,
-        f"Sensor de Fadiga / DMS R$ {dms36:,.2f}":dms36,
+        f"Videomonitoramento + DMS + ADAS R$ {mdvr36:,.2f}":mdvr36,
+        #f"Sensor de Fadiga / DMS R$ {dms36:,.2f}":dms36,
     }
     for item, valor in checkboxes.items():
         if st.checkbox(item):
