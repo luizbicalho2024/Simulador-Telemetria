86456b2e484bd62ddd607c18d325e8e3cc699cde
Bicalho <74017831+clientenocego@users.noreply.github.com>
2025-04-01 17:23:46 -0400

    1d3as25d416a5s4d6a5s14d

--- Simulador_Comercial.py
+++ Simulador_Comercial.py
@@ -13,4 +13,20 @@ st.image("imgs/logo.png", width=250)
 st.markdown("<h1 style='text-align: center; color: #54A033;'>Bem Vindo ao Simulador Verdio</h1>", unsafe_allow_html=True)
 st.markdown("<h3 style='text-align: center; color: #000;'>Comandos - Licitação - Pessoa Física - Pessoa Jurídica</h3>", unsafe_allow_html=True)
 st.markdown("---")
-st.image("imgs/bannerverdio.png", width=900, output_format="auto",  caption="")
+# CSS para centralizar imagens no Streamlit
+st.markdown(
+    """
+    <style>
+    .image-container {
+        display: flex;
+        justify-content: center;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Criar uma div com a classe "image-container" e usar st.image() dentro dela
+st.markdown('<div class="image-container">', unsafe_allow_html=True)
+st.image("imgs/bannerverdio.png", width=900, output_format="auto", caption="")
+st.markdown('</div>', unsafe_allow_html=True)
