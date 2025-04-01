e3cdef63e847f4536483f467cbad92d253cbea5f
Bicalho <74017831+clientenocego@users.noreply.github.com>
2025-04-01 17:17:00 -0400

    d4a65d1a65sd4adsasd

--- Simulador_Comercial.py
+++ Simulador_Comercial.py
@@ -13,4 +13,11 @@ st.image("imgs/logo.png", width=250)
 st.markdown("<h1 style='text-align: center; color: #54A033;'>Bem Vindo ao Simulador Verdio</h1>", unsafe_allow_html=True)
 st.markdown("<h3 style='text-align: center; color: #000;'>Comandos - Licitação - Pessoa Física - Pessoa Jurídica</h3>", unsafe_allow_html=True)
 st.markdown("---")
-st.image("imgs/bannerverdio.png", width=900, position="center", output_format="auto",  caption="")
+st.markdown(
+    """
+    <div style="display: flex; justify-content: center;">
+        <img src="imgs/bannerverdio.png" width="900">
+    </div>
+    """,
+    unsafe_allow_html=True
+)
